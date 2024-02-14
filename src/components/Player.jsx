da6d389321518774650c53f37e883c2aea47e373
AGCO92 <aguscossio@live.com.ar>
2024-02-14 13:36:40 -0300

    Add State to manage the playerName entered in the input and shows it in the Welcome message

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [playerName, setPlayerName] = useState(false);
+
+  const handleSetName = e => {
+    setEnteredPlayerName(e.target.value);
+  };
+
+  const handleName = () => {
+    setPlayerName(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={handleSetName} type="text" value={enteredPlayerName} />
+        <button onClick={handleName}>Set Name</button>
       </p>
     </section>
   );
